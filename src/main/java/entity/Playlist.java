1d6a0a1a5c8922113234e8b9225937f3693c2305
gcastalia <zhilin.gu@mail.utoronto.ca>
2025-11-10 18:27:32 -0500

    playlist entity elementary finished

---
+++ src/main/java/entity/Playlist.java
@@ -0,0 +1,32 @@
+package entity;
+
+/**
+ * An entity representing a playlist
+ */
+public class Playlist {
+    private String name;
+    private final String playlistId;
+    private final String userId;
+    private int songNumber;
+    private boolean selected;
+
+    public Playlist(String playlistId, String userId, String name, int songNumber) {
+        this.playlistId = playlistId;
+        this.userId = userId;
+        this.name = name;
+        this.songNumber = songNumber;
+        this.selected = false; // default: unselected
+    }
+
+    // getter 和 setter
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    // 其他 getter
+
+}
